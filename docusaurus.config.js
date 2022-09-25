eee955dd0e58ba1d1a270b9d3d12b9ea7208b45b
Harun Sokullu <harunsokullu@gmail.com>
2022-09-25 22:30:00 +0300

    add metadata

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,11 +14,6 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'suphero', // Usually your GitHub org/user name.
-  projectName: 'smart-stores-web', // Usually your repo name.
-
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -114,6 +109,12 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      metadata: [
+        {
+          name: 'Smart Stores',
+          content: 'Stores are smart with us'
+        }
+      ],
     }),
 };
 
